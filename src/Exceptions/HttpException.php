3437cf450b7fbe18fea0fd172c8a323e9c7834a6
ZeroXGod <s812419396@gmail.com>
2019-09-18 03:22:03 +0000

    first commit

---
+++ src/Exceptions/HttpException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Zerocoder\Weather\Exceptions;
+
+class HttpException extends Exception
+{
+
+}
---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Zerocoder\Weather\Exceptions;
+
+class InvalidArgumentException extends Exception
+{
+
+}
